99cd2336c380aae03087ce1de54dcd692f1ef837
ZionGate Cwb <andre.lgnd17165@gmail.com>
2025-01-23 02:39:58 -0300

    modified:   src/components/Footer.tsx 	modified:   src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 const Hero = () => {
   const Salvation = 'Compartilhar a mensagem de salvação de Jesus Cristo com todos.';
