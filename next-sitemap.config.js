56e2ca10897a4347487c523d85a660d03032ee91
ZionGate Cwb <andre.lgnd17165@gmail.com>
2025-01-28 01:20:21 -0300

    modified:   next-sitemap.config.js 	modified:   public/robots.txt 	modified:   public/sitemap-0.xml 	modified:   public/sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://caminhodavida.vercel.app/',
+  siteUrl: 'https://caminhodavida.vercel.app',
   exclude: ['/icon.svg', '/apple-icon.png', '/manifest.webmanifest', '/tags/*'],
   generateRobotsTxt: true,
-  generateIndexSitemap: true,
+  generateIndexSitemap: false,
   robotsTxtOptions: {
       policies: [
           {
